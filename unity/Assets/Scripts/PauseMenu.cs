27ef49ab43227232d71f03345c23496c422affc1
alilatoon <45373061+alilatoon@users.noreply.github.com>
2020-02-23 19:09:32 -0800

    Menu Stuff

---
+++ unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+        //  Debug.Log("The Game has Quit");
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Restart()
+    {
+       // GM.resetDict();
+        SceneManager.LoadScene("SampleScene");
+
+    }
+}
