ebf1fdee8edae5580385bc8979fb69cad44d6ca0
alilatoon <45373061+alilatoon@users.noreply.github.com>
2020-02-23 16:08:56 -0800

    Duplicate Scene

---
+++ unity/Assets/Scripts/DialogueOptions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialogueOption", menuName = "DialogeOption")]
+public class DialogueOptions : MonoBehaviour
+{
+    public string m_Dialogue;
+
+    public DialogueResponse[] m_responses;
+
+}
+[System.Serializable]
+public class DialogueResponse
+{
+    public string Response;
+    public DialogueOptions Option;
+    //Storing data
+                // public Evidence[] RequiredEvidence;
+}
