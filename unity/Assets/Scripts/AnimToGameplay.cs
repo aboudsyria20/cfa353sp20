19c21fd797c5dbf25572dcbf19c4147b7e4d785f
Kyle Tucker <ktucker@mail.bradley.edu>
2020-04-06 13:20:19 -0700

    Final Map and more

---
+++ unity/Assets/Scripts/AnimToGameplay.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AnimToGameplay : MonoBehaviour
+{
+  private float animTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+      StartCoroutine(GoToGameplay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(animTime > 21)
+        {
+
+        } */
+    }
+
+    IEnumerator GoToGameplay()
+    {
+        yield return new WaitForSeconds(21f);
+
+          SceneManager.LoadScene(2);
+    }
+}
